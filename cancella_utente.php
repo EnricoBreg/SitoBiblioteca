552d289447091921ac646e68ad1df2444fceb7ee
Enrico <bregolienrico@gmail.com>
2020-09-02 18:44:42 +0200

    cancellazione utente finita

---
+++ cancella_utente.php
@@ -0,0 +1,103 @@
+<?php
+
+    include_once("connection_test.php");
+
+    $Matricola = $_POST['Matricola'];
+    $affected_rows = 0;
+
+    $sql = "DELETE FROM STUDENTE
+            WHERE Matricola = '$Matricola'";
+    
+    mysqli_query($link, $sql);
+
+    $affected_rows = mysqli_affected_rows($link);
+
+    mysqli_close($link);
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Cancellazione utente - Biblioteca Universitaria</title>
+        <link rel="stylesheet" style="text/css" href="./myStyles.css">
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+    </head>
+
+    <body>
+
+        <div id="Titolo">
+            <p id="titolo">BIBLIOTECA UNIVERSITARIA DI FERRARA</p>
+            <a href="http://www.unife.it/it" target="blank"><img id="logo" src="./immagini/logo.jpg" alt="logo"></a>
+        </div>
+
+    <!-- inizio barra di navigazione -->
+    <div id="navBar">
+        <!-- *id="navBar"> -->
+        <ul>
+            <!-- dentro agli li ci andranno i link alle relative pagine -->
+            <li>
+                <a href="./index.html">Home</a>
+            </li>
+            <li><a href="http://www.unife.it/it/ateneo/strutture-uffici/dipartimenti-e-facolta" target="__blank">Dipartimenti</a></li>
+            <li><a href="./menu.html">Libri</a>
+                <ul>
+                    <li><a href="./book-sigin-1.php">Registra nuovo libro</a>
+                    <li><a>Registra nuova copia (libro esistente)</a></li>
+                    <li><a href="./all-books.php">Visualizza tutti i libri nel DB</a></li>
+                </ul>
+                </li>
+                <li><a href="./menu.html">Prestiti</a>
+                    <ul>
+                        <li><a href="./consulta-prestiti.html">Consulta situazione prestiti per codice matricola</a></li>
+                        <li><a href="./registra-prestito-1.php">Registra nuovo prestito</a></li>
+                        <li><a href="./registra_proroga.html">Proroga un prestito</a></li>
+                        <li><a href="./registra_rientro.html">Registra un rientro</a></li>
+                        <li><a href="./tutti-prestiti.php">Visualizza tutti i prestiti</a></li>
+                    </ul>
+                </li>
+                <li><a href="./menu.html">Utenti</a>
+                    <ul>
+                        <li><a href="./registra_utente.html">Registra nuovo utente</a></li>
+                        <li><a href="./aggiornamento_utente1.html">Aggiorna/Modifica utente</a></li>
+                        <li><a href="./cancella_utente.html">Cancella utente</a></li>
+                        <li><a href="./user-search.php">Ricerca uno studente</a></li>
+                    </ul>
+                </li>
+                <li><a href="./menu.html">Autori/Editori</a>
+                    <ul>
+                        <li><a href="./all-pubs.php">Visualizza tutti gli Editori dei libri</a></li>
+                        <li><a href="./all-authors.php">Visualizza tutti gli Autori dei libri</a></li>
+                        <li><a href="./pub-reg.php">Nuovo Editore</a></li>
+                        <li><a href="./author-reg.php">Nuovo Autore</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="usefull-info.php">Usefull Info</a>
+                </li>
+        </ul>
+    </div>
+    <!-- fine barra di navigazione -->
+        <!-- inizio form res -->
+        <div id="formRes">
+            <h1>ESITO CANCELLAZIONE UTENTE</h1>
+            <?php if($affected_rows > 0) {?>
+                <p>Cancellazione utente avvenuta con successo!</p>
+                <a href="./menu.html">Menù &#x2934;</a> | <a href="./index.html">Ritorna all HOME</a><br/>
+            <?php }
+            else { ?>
+                <p>OPS :(</p>
+                <p>Sembra che qualcosa sia andato storto durante la cancellazione.</p>
+                <p>ATTENZIONE!! Se l'utente inserito ha ancora prestiti attivi non è possibile cancellarlo.</p>
+                <p><a href="consulta-prestiti.html">Clicca qui</a> per vedere se l'utente ha prestiti attivi.</p>
+                <p>Se il problema persiste contatta l'amministratore</p>
+            <?php } ?>
+        </div>
+        <!-- fine form res -->
+
+    </body>
+
+</html>
